02e48076fe70c991de4b9cbb24451a59278d2e71
feremaitevscode <135708203+feremaitevscode@users.noreply.github.com>
2023-10-25 09:44:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-function tocaSom(idElmentoAudio){
+function tocaSom(idElementoAudio){
 document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 // Estrutura de repetição - Enquanto
-for(let contador = 0;contador < listaDeTeclas.length;contador++) {
+for(let contador = 0;contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
+  console.log(idAudio);
   
 
-    tecla.onclick = function (){
+    tecla.onclick = function(){
         tocaSom(idAudio);
     };
-
-    1;
+    tecla.onkeydown = function() {
+    tecla.classList.add('ativa');
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+console.log(contador);
 }
 
-const instrumento = listaDeTeclas[contador].classList[1];
 
-console.log(instrumento);
-listaDeTeclas[contador].onclick = function(){
-    tocaSom('#som_tecla_splash');
-}
